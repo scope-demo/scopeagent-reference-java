2bed6cc29ca6c103ed86d544e12ce68feea33d37
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2019-07-04 09:18:55 +0200

    Bump to ScopeAgent v0.1.7-beta.1

--- junit4-tests/src/test/java/com/undefinedlabs/scope/logging/SLF4JExamplesJUnit4Test.java
+++ junit4-tests/src/test/java/com/undefinedlabs/scope/logging/SLF4JExamplesJUnit4Test.java
@@ -6,9 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SLF4JExamplesJUnit4Test {
+    private static final String SAMPLE_MESSAGE_OTHER_EXECUTOR = "sampleMessage - OtherExecutor";
+    private static final String SAMPLE_MESSAGE_OTHER_THREAD = "sampleMessage - OtherThread";
 
     private static final String SAMPLE_MESSAGE = "sampleMessage";
     private static final String SAMPLE_PARAM = "sampleParam";
@@ -21,6 +26,102 @@ public class SLF4JExamplesJUnit4Test {
 
     private static final String SAMPLE_MARKER_TEXT = "sampleMarkerText";
     private static final Marker SAMPLE_MARKER = MarkerFactory.getMarker(SAMPLE_MARKER_TEXT);
+
+    @Test(timeout = 60000)
+    public void should_instrument_logger_thread_prop() throws InterruptedException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                log.info(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.debug(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.error(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.warn(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.trace(SAMPLE_MESSAGE_OTHER_THREAD);
+            }
+        };
+        final Thread thread = new Thread(runnable);
+
+        thread.start();
+        thread.join();
+    }
+
+    @Test(timeout = 60000)
+    public void should_instrument_logger_executor_prop() throws InterruptedException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+        final ExecutorService executor = Executors.newCachedThreadPool();
+        final Semaphore tested = new Semaphore(0);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                log.info(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.debug(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.error(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.warn(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.trace(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                tested.release();
+            }
+        });
+
+        tested.acquire();
+    }
+
+    @Test(timeout = 60000)
+    public void should_instrument_logger_executor_prop_submit_runnable() throws InterruptedException, ExecutionException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+        final ExecutorService executor = Executors.newFixedThreadPool(1);
+        final AtomicBoolean tested = new AtomicBoolean(false);
+
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                log.info(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.debug(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.error(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.warn(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.trace(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                tested.set(true);
+            }
+        }).get();
+
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                log.info(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.debug(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.error(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.warn(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.trace(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                tested.set(true);
+            }
+        }).get();
+
+        assertThat(tested.get()).isTrue();
+    }
+
+    @Test(timeout = 60000)
+    public void should_instrument_logger_executor_prop_submit_callable() throws InterruptedException, ExecutionException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+        final ExecutorService executor = Executors.newCachedThreadPool();
+        final AtomicBoolean tested = new AtomicBoolean(false);
+
+        executor.submit(new Callable<Void>() {
+            @Override
+            public Void call() {
+                log.info(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.debug(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.error(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.warn(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.trace(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                tested.set(true);
+                return null;
+            }
+        }).get();
+
+        assertThat(tested.get()).isTrue();
+    }
 
     @Test
     public void should_instrument_logger_info() {
--- junit5-tests/src/test/java/com/undefinedlabs/scope/logging/SLF4JExamplesJUnit5Test.java
+++ junit5-tests/src/test/java/com/undefinedlabs/scope/logging/SLF4JExamplesJUnit5Test.java
@@ -6,10 +6,15 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SLF4JExamplesJUnit5Test {
 
+    private static final String SAMPLE_MESSAGE_OTHER_EXECUTOR = "sampleMessage - OtherExecutor";
+    private static final String SAMPLE_MESSAGE_OTHER_THREAD = "sampleMessage - OtherThread";
     private static final String SAMPLE_MESSAGE = "sampleMessage";
     private static final String SAMPLE_PARAM = "sampleParam";
     private static final String WITH_MARKER = " with Marker";
@@ -21,6 +26,91 @@ public class SLF4JExamplesJUnit5Test {
 
     private static final String SAMPLE_MARKER_TEXT = "sampleMarkerText";
     private static final Marker SAMPLE_MARKER = MarkerFactory.getMarker(SAMPLE_MARKER_TEXT);
+
+    @Test
+    public void should_instrument_logger_info_thread_prop() throws InterruptedException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                log.info(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.debug(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.error(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.warn(SAMPLE_MESSAGE_OTHER_THREAD);
+                log.trace(SAMPLE_MESSAGE_OTHER_THREAD);
+            }
+        };
+        final Thread thread = new Thread(runnable);
+
+        thread.start();
+        thread.join();
+    }
+
+    @Test
+    public void should_instrument_logger_executor_prop() throws InterruptedException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+        final ExecutorService executor = Executors.newCachedThreadPool();
+        final Semaphore tested = new Semaphore(0);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                log.info(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.debug(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.error(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.warn(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.trace(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                tested.release();
+            }
+        });
+
+        tested.acquire();
+    }
+
+    @Test
+    public void should_instrument_logger_executor_prop_submit_runnable() throws InterruptedException, ExecutionException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+        final ExecutorService executor = Executors.newCachedThreadPool();
+        final AtomicBoolean tested = new AtomicBoolean(false);
+
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                log.info(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.debug(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.error(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.warn(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.trace(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                tested.set(true);
+            }
+        }).get();
+
+        assertThat(tested.get()).isTrue();
+    }
+
+    @Test
+    public void should_instrument_logger_executor_prop_submit_callable() throws InterruptedException, ExecutionException {
+        final Logger log = LoggerFactory.getLogger(this.getClass());
+        final ExecutorService executor = Executors.newCachedThreadPool();
+        final AtomicBoolean tested = new AtomicBoolean(false);
+
+        executor.submit(new Callable<Void>() {
+            @Override
+            public Void call() {
+                log.info(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.debug(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.error(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.warn(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                log.trace(SAMPLE_MESSAGE_OTHER_EXECUTOR);
+                tested.set(true);
+                return null;
+            }
+        }).get();
+
+        assertThat(tested.get()).isTrue();
+    }
+
 
     @Test
     public void should_instrument_logger_info() {
